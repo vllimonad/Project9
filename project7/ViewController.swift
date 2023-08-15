b07e00e40c3e39d1029e4c729fa0389d4872a52b
Vlad Klunduk <klundukvlad40@gmail.com>
2023-08-15 18:24:08 +0200

    Initial Commit

---
+++ project7/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Vlad Klunduk on 14/08/2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title is here"
+        cell.detailTextLabel?.text = "details are here"
+        return cell
+    }
+}
+

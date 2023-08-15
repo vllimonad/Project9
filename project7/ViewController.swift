c51aeb48642ceebdac1475c25c18357c6bd5270b
Vlad Klunduk <klundukvlad40@gmail.com>
2023-08-15 22:29:26 +0200

    topRated tabBarItem

--- project7/ViewController.swift
+++ project7/ViewController.swift
@@ -14,13 +14,21 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0{
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString){
             if let data = try? Data(contentsOf: url){
                 parse(data)
+                return
             }
         }
+        showError()
     }
     
     func parse(_ json: Data){
@@ -49,6 +57,12 @@ class ViewController: UITableViewController {
         let vc = DetailViewController()
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func showError(){
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
